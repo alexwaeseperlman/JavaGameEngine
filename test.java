bf7301343db313ea8547924ff8f7b46126080a76
Alex Waese-Perlman <squishybanana04@gmail.com>
2017-06-02 23:30:20 -0400

    Update tests

--- test.java
+++ test.java
@@ -1,18 +1,23 @@
+import org.joml.*;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.*;
 
 import engine.*;
+import engine.Engine.*;
 import engine.Graphics.*;
+
+import java.lang.Math;
 
 class test extends App {
 	Shader v;
+	Shader s;
 	VBO vbo;
 	VAO vao;
 	Texture tex;
+	Camera cam = new Camera();
 
 	public static void main(String[] args) {
 		new test();
-		System.out.println("Hello world");
 	}
 
 	@Override
@@ -37,20 +42,32 @@ class test extends App {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		vbo = new VBO(new float[][] { { -1, -1 }, { -1, 1 }, { 1, 1 }, { 1, -1 } });
+		vbo = new VBO(new float[][] { { -0.5f, -0.5f }, { -0.5f, 0.5f }, { 0.5f, 0.5f }, { 0.5f, -0.5f } });
 		vao = new VAO(vbo);
 		v = new Shader(
-				"#version 330\nuniform float passedTime;\nout vec2 position;\nlayout(location = 0)in vec2 vecPos; void main() { gl_Position = vec4(vecPos.x + sin(passedTime * 2.2), vecPos.y, 0, cos(passedTime * 3) / 2 + 1); position.xy = vecPos.xy; }",
+				"#version 330\nuniform float passedTime;uniform mat4 modelViewProjection;\nout vec2 position;\nlayout(location = 0)in vec2 vecPos; void main() { gl_Position = vec4(vecPos.x + sin(passedTime * 2.2), vecPos.y, 0, cos(passedTime * 3) / 2 + 1); position.xy = vecPos.xy; }",
 				"#version 130\n uniform float passedTime; uniform sampler2D img; in vec2 position; out vec4 color; void main() { color = vec4(texture(img, vec2(position.x * sin(passedTime), position.y * sin(sin(passedTime * 1.1)))).rg, sin(passedTime), 1); }");
+		s = new Shader(
+				"#version 330\nuniform mat4 modelViewProjection;\nlayout(location = 0) in vec2 vecPos; void main() { gl_Position = modelViewProjection * vec4(vecPos.xy, 1, 1); } ",
+				"#version 130\n out vec4 color; void main() { color = vec4(0, 1, 0, 1); }");
+
 	}
 
 	@Override
 	public void draw() {
-		v.uniform.setFloat1("passedTime", time.elapsedTime);
-		GL11.glClearColor(0, 0, 0, 0.1f);
+		Vector3f position = new Vector3f(-0.5f, -0.5f, 1);
+		cam.getMatrix().transformPosition(position);
+		System.out.println(position.toString());
+		GL11.glClearColor(0, 0, 0, 0f);
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-		v.bind();
-		vao.draw(Renderer.drawModes.TRIANGLE_STRIP, 0, 3);
+		cam.setTranslation(0.3f, 0, -1);
+		cam.perspective();
+		cam.setFar(10);
+		s.bind();
+		s.uniform.setMatrix4("modelViewProjection", cam.getMatrix());
+		vao.draw(Renderer.drawModes.TRIANGLE_STRIP, 0, 5);
+
+		System.out.printf("Camera: %s\n", cam.getProjectionMatrix().toString());
 	}
 
 }
